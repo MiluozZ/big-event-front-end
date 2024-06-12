4396d426db7734ffcded53981e741fc1175c27ae
luozx <604679490@qq.com>
2024-06-12 11:36:07 +0800

    feat：新增文章分类

--- src/views/article/ArticleCategory.vue
+++ src/views/article/ArticleCategory.vue
@@ -3,6 +3,7 @@ import {
   Edit,
   Delete
 } from '@element-plus/icons-vue'
+import { ElMessage } from 'element-plus'
 import {ref} from "vue";
 import {
   addCategoryService,
@@ -12,19 +13,35 @@ import {
   categoryListService} from "@/api/category";
 
 const categoryList = ref([])
-const categoryReqData = ref([{
-  id: '',
+const categoryReqData = ref({
   categoryName: '',
   categoryAlias: ''
-}])
+})
 
 const getAllCategoryList = async () => {
   let result = await categoryListService()
   categoryList.value = result.data
 }
-getAllCategoryList().catch(err => {
-  console.log(err)
-});
+getAllCategoryList()
+
+// 新增文章分类
+const dialogVisible = ref(false);
+const addCategoryRule = {
+  categoryName: [
+    {required: true, message: '请输入分类名称', trigger: 'blur' },
+  ],
+  categoryAlias: [
+    {required: true, message: '请输入分类别名', trigger: 'blur' },
+  ]
+}
+const addCategory = async () => {
+  let result = await addCategoryService(categoryReqData.value)
+  ElMessage.success(result.message ? result.message : '添加成功')
+  dialogVisible.value = false
+  getAllCategoryList()
+}
+
+
 
 </script>
 <template>
@@ -33,7 +50,7 @@ getAllCategoryList().catch(err => {
       <div class="header">
         <span>文章分类</span>
         <div class="extra">
-          <el-button type="primary">添加分类</el-button>
+          <el-button type="primary" @click="dialogVisible=true">添加分类</el-button>
         </div>
       </div>
     </template>
@@ -52,6 +69,23 @@ getAllCategoryList().catch(err => {
       </template>
     </el-table>
   </el-card>
+
+  <el-dialog v-model="dialogVisible" title="添加文章分类" width="30%">
+    <el-form :model="categoryReqData" :rules="addCategoryRule" label-width="100px" style="padding-right: 30px">
+      <el-form-item label="分类名称" prop="categoryName">
+        <el-input v-model="categoryReqData.categoryName" minlength="1" maxlength="10"></el-input>
+      </el-form-item>
+      <el-form-item label="分类别名" prop="categoryAlias">
+        <el-input v-model="categoryReqData.categoryAlias" minlength="1" maxlength="15"></el-input>
+      </el-form-item>
+    </el-form>
+    <template #footer>
+        <span class="dialog-footer">
+            <el-button @click="dialogVisible = false">取消</el-button>
+            <el-button type="primary" @click="addCategory"> 确认 </el-button>
+        </span>
+    </template>
+  </el-dialog>
 </template>
 <style lang="scss" scoped>
 .page-container {
